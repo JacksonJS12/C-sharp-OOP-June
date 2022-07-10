d85390db0645789f3d8dbd5f64d8f796bff97f39
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-10 21:18:02 +0300

    Commit.

--- 02.ExerciseInheritance/P05.Restaurant/Beverages.cs
+++ 02.ExerciseInheritance/P05.Restaurant/Beverages.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace Restaurant
 {
-    public class Beverages
+    public class Beverages : Product
     {
+        public Beverages(string name, decimal price, double milliliters) : base(name, price)
+        {
+            this.Milliliters = milliliters;
+        }
+        public double Milliliters  { get; set; }
     }
 }
