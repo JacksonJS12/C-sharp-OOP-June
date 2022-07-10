da8fd6ddb368df696f35513c861a2748399615b2
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-10 21:45:24 +0300

    All animals are created.

---
+++ 02.ExerciseInheritance/P06.Animals/Kittens.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Kittens : Cat
+    {
+        public Kittens(string name, int age, string gender) 
+            : base(name, age, gender)
+        {
+        }
+        public override string ProduceSound()
+        {
+            return "Meow";
+        }
+    }
+}
