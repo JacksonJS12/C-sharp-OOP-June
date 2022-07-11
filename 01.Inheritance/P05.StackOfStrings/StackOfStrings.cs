5120d0323f37fe8d24fc7c4da8a7666ce79c1a1a
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-11 14:05:58 +0300

    100/100

---
+++ 01.Inheritance/P05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            Stack<string> stack = new Stack<string>();
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public Stack<string> AddRange()
+        {
+            Stack<string> stack = new Stack<string>();
+            stack.Push(string.Empty);
+            return stack;
+        }
+    }
+}
