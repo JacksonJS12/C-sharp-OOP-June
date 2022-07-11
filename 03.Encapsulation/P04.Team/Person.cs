8ad9f433636100178dd3709c56b402ed05790ca7
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-11 13:23:56 +0300

    Ex 04 100/100

---
+++ 03.Encapsulation/P04.Team/Person.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName 
+        { 
+            get
+            {
+                return firstName;
+            }
+            private set
+            {
+                if (value.Length > 3)
+                {
+                    firstName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            private set
+            {
+                if (value.Length > 3)
+                {
+                    lastName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+            }
+        }
+        public decimal Salary {
+            get
+            {
+                return salary;
+            }
+            private set
+            {
+                if (value > 460)
+                {
+                    salary = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+            }
+        }
+       
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age >= 30)
+            {
+                this.Salary += percentage * this.Salary / 100;
+            }
+            else
+            {
+                this.Salary += percentage * this.Salary / 200;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+    }
+}
