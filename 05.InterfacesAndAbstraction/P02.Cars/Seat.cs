51855c4307b9c00f0125ebc9f6947b9df759c364
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-13 13:02:40 +0300

    100/100

---
+++ 05.InterfacesAndAbstraction/P02.Cars/Seat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : Car
+    {
+        public Seat(string model, string color) 
+            : base(model, color)
+        {
+        }
+
+        public override void Run()
+        {
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            result
+                .AppendLine(base.ToString())
+                .AppendLine(this.Start())
+                .AppendLine(this.Stop());
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 05.InterfacesAndAbstraction/P02.Cars/Tesla.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class 
+        Tesla : Car, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+            : base(model, color)
+        {
+            this.Battery = battery;
+        }
+
+        public int Battery { get; private set; }
+
+        public void Crarge()
+        {
+            this.Battery++;
+        }
+
+        public override void Run()
+        {
+            this.Battery--;
+        }
+
+        public override string Start()
+        {
+            return "Engine start";
+        }
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            result
+                .AppendLine($"{base.ToString()} with {this.Battery} batteries")
+                .AppendLine(this.Start())
+                .AppendLine(this.Stop());
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
