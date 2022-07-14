706e9bf1afa468517664956fd37d3144e63ca648
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-15 00:36:55 +0300

    Input-Output folder added

---
+++ 06.ExerciseInterfacesAndAbstraction/P03.Telephony/Input-Output/Interfaces/IReader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telephony.Input_Output.Interfaces
+{
+    public interface IReader
+    {
+        public string ReadLine();
+    }
+}
