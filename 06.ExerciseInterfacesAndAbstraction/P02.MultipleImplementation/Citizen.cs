4b6f56f80f7f0bb2404d11197c93db8838b5e4a1
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-14 23:41:21 +0300

    100/100

---
+++ 06.ExerciseInterfacesAndAbstraction/P02.MultipleImplementation/Citizen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IBirthable, IIdentifiable
+    {
+        private int age;
+        private string name;
+
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Age = age;
+            this.Name = name;
+            this.Birthdate = birthdate;
+            this.Id = id;
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+             set
+            {
+                this.age = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public string Id { get; private set; }
+
+        public string Birthdate { get; private set; }
+    }
+}
---
+++ 06.ExerciseInterfacesAndAbstraction/P02.MultipleImplementation/Interfaces/IPerson.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public interface IPerson
+    {
+        int Age { get; set; }
+        string Name { get; set; }
+    }
+}
