d84056a341ae4be5c85226a7073e0764fcd762b7
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-14 23:56:45 +0300

    Commit.

---
+++ 06.ExerciseInterfacesAndAbstraction/P03.Telephony/Smartphone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telephony.Interfaces;
+
+namespace Telephony
+{
+    public class Smartphone : ISmartphone
+    {
+        public void Browsing()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Calling()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 06.ExerciseInterfacesAndAbstraction/P03.Telephony/StationaryPhone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telephony.Interfaces;
+
+namespace Telephony
+{
+    public class StationaryPhone : IStationary
+    {
+        public void Call()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
