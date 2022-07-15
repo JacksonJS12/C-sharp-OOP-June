a443f4910cf0b154ab8b4302efb217d313790ca8
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-15 13:52:55 +0300

    Added Interfaces

---
+++ 06.ExerciseInterfacesAndAbstraction/P08.CollectionHierarchy/Interfaces/IAddCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionHierarchy.Interfaces
+{
+    public interface IAddCollection<T>
+    {
+        public int Add(T item);
+    }
+}
