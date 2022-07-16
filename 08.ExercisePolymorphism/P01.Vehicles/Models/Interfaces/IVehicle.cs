3486b39fc5e48c16cc8d3f4c2e00b971e996a9f5
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-16 22:56:35 +0300

    Commit.

---
+++ 08.ExercisePolymorphism/P01.Vehicles/Models/Interfaces/IVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicle.Models.Interfaces
+{
+    public interface IVehicle
+    {
+        public double FuelQuantity { get; }
+        public double FuelConsumption { get; }
+
+        public string Drive(double disctance);
+        public void Refuel(double liters);
+    }
+}
