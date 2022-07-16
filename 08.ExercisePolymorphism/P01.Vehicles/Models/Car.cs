45ac8fe3d615bb2147704cec76e88aafa5d8b93b
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-16 22:56:50 +0300

    Commit.

---
+++ 08.ExercisePolymorphism/P01.Vehicles/Models/Car.cs
@@ -0,0 +1,29 @@
+﻿namespace Vehicle.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Interfaces;
+    public class Car : Vehicle
+    {
+        public Car(double fuelQuantity, double fuelConsuption)
+            : base(fuelQuantity, fuelConsuption)
+        {
+        }
+
+        public override double FuelConsumption
+        {
+            get
+            {
+                return base.FuelConsumption;
+            }
+            protected set
+            {
+                base.FuelConsumption = value + this.FuelConsumptionIncrement;
+            }
+        }
+
+        public override double FuelConsumptionIncrement
+            => 0.9;
+    }
+}
---
+++ 08.ExercisePolymorphism/P01.Vehicles/Models/Truck.cs
@@ -0,0 +1,33 @@
+﻿namespace Vehicle.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Interfaces;
+    public class Truck : Vehicle
+    {
+        public Truck(double fuelQuantity, double fuelConsuption)
+            : base(fuelQuantity, fuelConsuption)
+        {
+        }
+
+        public override double FuelConsumption 
+        {
+            get
+            {
+                return base.FuelConsumption;
+            }
+            protected set
+            {
+                this.FuelConsumption = value + this.FuelConsumptionIncrement;
+            }
+        }
+
+        public override double FuelConsumptionIncrement
+            => 1.6;
+        public override void Refuel(double liters)
+        {
+            base.Refuel(liters * 0.95);
+        }
+    }
+}
