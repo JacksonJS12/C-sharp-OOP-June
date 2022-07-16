319d1339af478630b50581b8aff64eb8c7397021
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-16 22:57:26 +0300

    Commit.

---
+++ 08.ExercisePolymorphism/P01.Vehicles/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace P01.Vehicles
+{
+    using System;
+    using Vehicle.Core;
+    using Vehicle.Factories;
+    using Vehicle.Models;
+    using Vehicle.Models.Interfaces;
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] carData = Console.ReadLine()
+                .Split();
+            string[] truckData = Console.ReadLine()
+                .Split();
+
+            IVehicleFactory vehicleFacoty = new VehicleFactory();
+            Vehicle car = vehicleFacoty
+                .CreateVehicle(carData[0], double.Parse(carData[1]), double.Parse(carData[2]));
+            Vehicle truck = vehicleFacoty
+                .CreateVehicle(carData[0], double.Parse(carData[1]), double.Parse(carData[2]));
+
+            IEngine engine = new Engine(car, truck);
+            engine.Start();
+        }
+    }
+}
