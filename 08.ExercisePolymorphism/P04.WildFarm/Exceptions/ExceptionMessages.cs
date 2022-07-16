ae027981d1c84aeef8f689a9256db808bdcd7277
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-17 01:24:36 +0300

    Commit.

---
+++ 08.ExercisePolymorphism/P04.WildFarm/Exceptions/ExceptionMessages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04.WildFarm
+{
+    public class ExceptionMessages
+    {
+        public const string FoodNotPreffered = "{0} does not eat {1}!";
+    }
+}
---
+++ 08.ExercisePolymorphism/P04.WildFarm/Exceptions/FoodNotPreferredException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace P04.WildFarm
+{
+    public class FoodNotPreferredException : Exception
+    {
+        public FoodNotPreferredException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
