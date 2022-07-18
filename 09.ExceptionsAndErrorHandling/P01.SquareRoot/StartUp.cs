5ec4e88ecb07db19bc9fb0f2cc508d37be96f420
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-18 12:37:09 +0300

    Commit.

---
+++ 09.ExceptionsAndErrorHandling/P01.SquareRoot/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01.SquareRoot
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
