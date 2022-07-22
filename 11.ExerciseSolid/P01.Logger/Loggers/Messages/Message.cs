f7df444eef4a42545fd296569cfdb93d1bcd1fbe
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-22 16:39:55 +0300

    Commit.

---
+++ 11.ExerciseSolid/P01.Logger/Loggers/Messages/Message.cs
@@ -0,0 +1,21 @@
+﻿namespace P01.Logger
+{ 
+    public class Message : IMessage
+    {
+        private string dateTime;
+        private string messageText;
+
+        public Message(string dateTime, string messageText, ReportLevel level)
+        {
+            this.DateTime = dateTime;
+            this.MessageText = messageText;
+            this.Level = level;
+        }
+
+        public string DateTime { get; }
+        public string MessageText { get; }
+        public ReportLevel Level { get; }
+
+        
+    }
+}
