635b09f4851a0061e15960088b7c920fdeb7449e
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-24 02:22:47 +0300

    Commit.

---
+++ 11.ExerciseSolid/P01.Logger/Loggers/Interfaces/ILogger.cs
@@ -0,0 +1,14 @@
+﻿namespace SoftUniLogger
+{
+    using System.Collections.Generic;
+
+
+    partial interface ILogger
+    {
+        void Info(string logTime, string message, ReportLevel level);
+        void Warning(string logTime, string message, ReportLevel level);
+        void Error(string logTime, string message, ReportLevel level);
+        void Crirical(string logTime, string message, ReportLevel level);
+        void Fatal(string logTime, string message, ReportLevel level);
+    }
+}
---
+++ 11.ExerciseSolid/P01.Logger/Loggers/Logger.cs
@@ -0,0 +1,61 @@
+﻿namespace SoftUniLogger
+{
+    using System.Collections.Generic;
+
+
+    public class Logger : IAppenderCollection, ILogger
+    {
+        private readonly ICollection<IAppender> appenders;
+        private Logger()
+        {
+            this.appenders = new HashSet<IAppender>();
+        }
+        public Logger(params IAppender[] appenders)
+            : this()
+        {
+            this.appenders.AddRange(appenders);
+        }
+        public IReadOnlyCollection<IAppender> Appenders
+            => this.appenders.AsReadOnly();
+
+        public void AddAppender(IAppender appender)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Crirical(string logTime, string message, ReportLevel level)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Error(string logTime, string message, ReportLevel level)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Fatal(string logTime, string message, ReportLevel level)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Info(string logTime, string message, ReportLevel level)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool RemoveAppender(IAppender appender)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Warning(string logTime, string message, ReportLevel level)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
