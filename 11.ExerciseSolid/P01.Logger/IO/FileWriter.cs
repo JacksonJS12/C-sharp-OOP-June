9cf4db1a3dbc645a275dcdd0cc0b8278d0ce0d44
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-24 01:51:34 +0300

    IO folder done

---
+++ 11.ExerciseSolid/P01.Logger/IO/FileWriter.cs
@@ -0,0 +1,21 @@
+﻿namespace SoftUniLogger
+{
+    using System.IO;
+    using System.Text;
+
+    partial class FileWriter : IFileWriter
+    {
+        public FileWriter(string filePath)
+        {
+            this.FilePath = filePath;
+        }
+        public string FilePath { get; set; }
+
+        public void WriteContent(string content, string fileName)
+        {
+            string outputPath = Path.Combine(this.FilePath, fileName);
+
+            File.WriteAllText(outputPath, content, Encoding.UTF8);
+        }
+    }
+}
