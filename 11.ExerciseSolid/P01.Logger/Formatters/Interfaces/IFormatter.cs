bad61e188231cf4f2ee98a68d4914e19d89adb67
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-24 00:56:53 +0300

    Fixed the format message

---
+++ 11.ExerciseSolid/P01.Logger/Formatters/Interfaces/IFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniLogger
+{
+    internal interface IFormatter 
+    {
+        ILayout Layout { get; }
+        string FormatMessage(IMessage message);
+    }
+}
