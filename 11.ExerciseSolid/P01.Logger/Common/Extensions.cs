1bc5271a04607775e6f73fc7108571c40bc6fc27
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-24 02:22:35 +0300

    Commit.

---
+++ 11.ExerciseSolid/P01.Logger/Common/Extensions.cs
@@ -0,0 +1,16 @@
+﻿namespace SoftUniLogger
+{
+    using System.Collections.Generic;
+    public static class Extensions
+    {
+        public static void AddRange(this ICollection<IAppender> appenders, IEnumerable<IAppender> appendersToAdd)
+        {
+            foreach (IAppender apToAdd in appendersToAdd)
+            {
+                appenders.Add(apToAdd);
+            }
+        }
+        public static IReadOnlyCollection<IAppender> AsReadOnly(this ICollection<IAppender> appenders)
+        => (IReadOnlyCollection<IAppender>)appenders;
+    }
+}
