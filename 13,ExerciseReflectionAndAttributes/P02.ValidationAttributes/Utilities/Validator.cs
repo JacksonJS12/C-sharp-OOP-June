be09cf2a3c265ad2e8f39765edcd631f5757d3f2
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-29 00:54:55 +0300

    02 Update

---
+++ 13,ExerciseReflectionAndAttributes/P02.ValidationAttributes/Utilities/Validator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ValidationAttributes.Utilities
+{
+    public static class Validator 
+    {
+        public static bool IsValid(object obj)
+        {
+           // var validationContext = new ValidationContext(obj);
+           // ICollection<ValidationResult> errors = new List<ValidationResult>();
+
+           //return System.ComponentModel.DataAnnotations.Validator.TryValidateObject(obj, validationContext, errors);
+        }
+    }
+}
