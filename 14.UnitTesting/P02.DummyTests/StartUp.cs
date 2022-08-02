c4943d81f143ead9a82c11e05f8b5bce7909ead6
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-08-02 22:25:30 +0300

    Fix the skeleton

---
+++ 14.UnitTesting/P02.DummyTests/StartUp.cs
@@ -0,0 +1,10 @@
+﻿public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Axe axe = new Axe(10, 10);
+        Dummy dummy = new Dummy(100, 100);
+
+        axe.Attack(dummy);
+    }
+}
