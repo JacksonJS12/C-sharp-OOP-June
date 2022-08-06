0155bc66069738036511d0711b18ee621f89ba7c
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-08-06 17:29:32 +0300

    Commit

---
+++ 15.ExerciseUnitTesting/P01.Database/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04.FightingArenaTests
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
