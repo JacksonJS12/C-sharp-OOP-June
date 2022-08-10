2fd1162ae9a81a5afc8f2d6fe5a2b1b9ee4b298b
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-08-10 16:14:55 +0300

    Exercise completed

---
+++ 19.ExerciseDesignPatterns/P01.PrototypePattern/Sandwich.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P01.PrototypePattern
+{
+    public class Sandwich : SandwichPrototype
+    {
+        private readonly string bread;
+        private readonly string meat;
+        private readonly string chease;
+        private readonly string veggies;
+
+        public Sandwich(string bread, string meat, string chease, string veggies )
+        {
+            this.bread = bread;
+            this.meat = meat;
+            this.chease = chease;
+            this.veggies = veggies;
+        }
+
+        public override SandwichPrototype Clone()
+        {
+            string ingredients = this.GetIngreadientsList();
+            //Good to implement Reader
+            Console.WriteLine($"Cloning sandwich with ingredients: {ingredients}");
+
+            return (this.MemberwiseClone() as SandwichPrototype)!;
+        }
+        private string GetIngreadientsList()
+        {
+            return $"{this.bread}, {this.meat}, {this.chease}, {this.veggies}";
+        }
+    }
+}
---
+++ 19.ExerciseDesignPatterns/P01.PrototypePattern/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace P01.PrototypePattern
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SandwichMenu menu = new SandwichMenu();
+
+            //Default sandwiches
+            menu["BLT"] = 
+                new Sandwich("Wheat", "Bacon", "", "Lettuce, Tomato");
+            menu["PB&J"] = 
+                new Sandwich("White", "", "", "Peanut butter, Jelly");
+            menu["Turkey"] = 
+                new Sandwich("Rye", "Turkey", "Swiss", "Lettuce, Onion, Tomato");
+
+            //Deli manager adds custom sandwiches
+            menu["LoadedBLT"] =
+                new Sandwich("Wheat", "Turkey, Bacon", "American", "Lettuce, Onion, Tomato, Olives");
+            menu["ThreeMeatCombo"] =
+               new Sandwich("Rye", "Turkey, Ham, Salami", "Provolone", "Lettuce, Onion");
+            menu["Vegetarian"] =
+               new Sandwich("Wheat", "", "", "Lettuce, Onion, Tomato, Olives, Spinach");
+
+            //Now we can clone these sandwiches
+            Sandwich sandwich1 = (menu["BLT"].Clone() as Sandwich)!;
+            Sandwich sandwich2 = (menu["ThreeMeatCombo"].Clone() as Sandwich)!;
+            Sandwich sandwich3 = (menu["Vegetarian"].Clone() as Sandwich)!;
+
+        }
+    }
+}
