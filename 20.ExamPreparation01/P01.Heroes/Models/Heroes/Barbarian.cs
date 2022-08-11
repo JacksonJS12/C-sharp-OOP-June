4be700aca7c299e7a689872f7429d4b17d630ca3
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-08-11 12:38:26 +0300

    Heroes and weapons classes are done

---
+++ 20.ExamPreparation01/P01.Heroes/Models/Heroes/Barbarian.cs
@@ -0,0 +1,15 @@
+﻿using Heroes.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Heroes.Models.Heroes
+{
+    public class Barbarian : Hero
+    {
+        public Barbarian(string name, int health, int armour) 
+            : base(name, health, armour)
+        {
+        }
+    }
+}
---
+++ 20.ExamPreparation01/P01.Heroes/Models/Weapons/Mace.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Heroes.Models.Weapons
+{
+    public class Mace : Weapon
+    {
+        private const int Damage = 25; 
+        public Mace(string name, int durability, int damage) 
+            : base(name, durability, Damage)
+        {
+        }
+
+        
+    }
+}
