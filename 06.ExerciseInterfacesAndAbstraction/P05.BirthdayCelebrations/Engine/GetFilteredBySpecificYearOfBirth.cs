d03cc66b3078138963aa9438694fdf0ddffa3c17
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-08-12 10:42:26 +0300

    Added exercises solution

---
+++ 06.ExerciseInterfacesAndAbstraction/P05.BirthdayCelebrations/Engine/GetFilteredBySpecificYearOfBirth.cs
@@ -0,0 +1,29 @@
+﻿namespace BirthdayCelebrations.Engine
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Models;
+    public class GetFilteredBySpecificYearOfBirth
+    {
+
+        public ListOfFilteredBySpecificYear Filter(List<Humanoid> humanoids, string lastDigitsOfFakeIds)
+        {
+            var listOfDetainedIds = new List<string>();
+            foreach (var humanoid in humanoids)
+            {
+
+                if (humanoid.Birthdate.EndsWith(lastDigitsOfFakeIds))
+                {
+                    listOfDetainedIds.Add(humanoid.Birthdate);
+                }
+            }
+
+            return new ListOfFilteredBySpecificYear
+            {
+                specifiedListOfBirthdates = listOfDetainedIds
+            };
+        }
+
+    }
+}
