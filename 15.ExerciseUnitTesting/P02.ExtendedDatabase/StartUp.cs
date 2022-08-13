7d21fcc0d9a90423bc14f62ca287b5a103d3ed6b
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-08-13 14:10:23 +0300

    Commit.

---
+++ 15.ExerciseUnitTesting/P02.ExtendedDatabase/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02.ExtendedDatabase
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
