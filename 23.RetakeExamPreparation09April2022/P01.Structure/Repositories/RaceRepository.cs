7d809c55b4e88c200948fe5482cda028f62d3e59
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-08-22 01:54:39 +0300

    45/100

---
+++ 23.RetakeExamPreparation09April2022/P01.Structure/Repositories/RaceRepository.cs
@@ -0,0 +1,30 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Formula1.Repositories
+{
+    public class RaceRepository : IRepository<IRace>
+    {
+        private readonly ICollection<IRace> models;
+        private RaceRepository()
+        {
+            this.models = new List<IRace>();
+        }
+        public IReadOnlyCollection<IRace> Models
+            => (IReadOnlyCollection<IRace>)this.models;
+
+        public void Add(IRace pilot)
+            => this.models.Add(pilot);
+
+        public IRace FindByName(string name)
+            => this.models.FirstOrDefault(r => r.RaceName == name);
+
+
+        public bool Remove(IRace model)
+            => this.models.Remove(model);
+    }
+}
