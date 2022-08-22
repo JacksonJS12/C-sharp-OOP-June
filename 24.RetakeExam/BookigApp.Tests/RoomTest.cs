460594aa5f28c96a6d651f0f006402999b35d74e
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-08-22 09:31:50 +0300

    Init

---
+++ 24.RetakeExam/BookigApp.Tests/RoomTest.cs
@@ -0,0 +1,31 @@
+using FrontDeskApp;
+using NUnit.Framework;
+using System;
+
+namespace BookigApp.Tests
+{
+    [TestFixture]
+    public class RoomTests
+    {
+        [Test]
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(999)]
+        public void TestIfConstructorSetsBedCapacityCorrectly(int bedCapacity)
+        {
+            Room room = new Room(bedCapacity, 10);
+
+            Assert.AreEqual(room.BedCapacity, bedCapacity);
+        }
+        [Test]
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(999)]
+        public void TestIfConstructorSetsPricePerNightCorrectly(double pricePerNight)
+        {
+            Room room = new Room(20, pricePerNight);
+
+            Assert.AreEqual(room.PricePerNight, pricePerNight);
+        }
+    }
+}
